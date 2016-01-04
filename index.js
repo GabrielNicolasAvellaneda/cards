ba4018cd2687174534ac0bb3cd0d5ff46b2acf65
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2016-01-04 15:06:59 -0300

    Assign a player to a request
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
+//var session = require('express-session');
+
 var app = express();
 
-var totalRequests = 0;
-
+var stats = {};
 var players = [];
 
 var getRandomName = function () {
     return "Player " + players.length;
 };
 
+var playerExists = function (player) {
+    return players.indexOf(player) != -1;
+};
+
+var isUndefined = function (x) {
+    return x == undefined;
+};
+
+app.set('trust proxy', 1);
 app.use(cookieParser());
 
 app.get('/', function (req, res) {
     console.log(req.cookies);
-    totalRequests++;
-    var player = getRandomName();
-    players.push(player);
-    res.send('You are the player: ' + player);
+    stats.requets = (stats.requests || 0) + 1;
+    var player = req.cookies.player;
+    if (isUndefined(player) || !playerExists(player)) {
+        player = getRandomName();
+        players.push(player);
+        res.cookie('player', player, {maxAge: 900000, httpOnly: true});
+        console.log("Player defined as " + player);
+    }
 
+    res.send('You are the player: ' + player);
 });
 
 var server = app.listen(3000, function () {
